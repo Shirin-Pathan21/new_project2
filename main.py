9a3ebc6959b20ea9343a9b3092ede38b612e90bd
unknown <shirinkhan446@gmail.com>
2023-01-18 11:40:17 +0530

    Added

---
+++ main.py
@@ -0,0 +1,18 @@
+#detail program
+#detail program
+
+
+import phonenumbers
+from test import number
+
+
+# get location
+from phonenumbers import geocoder
+ch_number = phonenumbers.parse(number, "CH")
+print(geocoder.description_for_number(ch_number, "en"))
+
+
+# get service provider
+from phonenumbers import carrier
+service_number = phonenumbers.parse(number, "RO")
+print(carrier.name_for_number(service_number, "en"))
