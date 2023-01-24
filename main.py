64a361dc389380bcada3d5e882ff7280f8ce9487
unknown <shirinkhan446@gmail.com>
2023-01-24 13:15:48 +0530

    updated main file

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 #detail program
 #detail program
 
+###############################################
 
 import phonenumbers
 from test import number
 
+##############################################
 
 # get location
 from phonenumbers import geocoder
 ch_number = phonenumbers.parse(number, "CH")
 print(geocoder.description_for_number(ch_number, "en"))
 
+###############################################
 
 # get service provider
 from phonenumbers import carrier
